a125b7ca386c964a8116e1d4d49c03fc61f02ea3
monkeyx <monkeyx@gmail.com>
2013-07-16 17:24:18 +0100

    Moving argument from module to class

--- lib/generators/pbw/area/area_generator.rb
+++ lib/generators/pbw/area/area_generator.rb
@@ -4,5 +4,7 @@ class Pbw::AreaGenerator < Rails::Generators::NamedBase
 	
 	source_root File.expand_path("../templates", __FILE__)
 
+	argument :attributes, :type => :array, :default => [], :banner => "field:type field:type"
+
 	include Pbw::Generators::ScaffoldHelpers
 end
--- lib/generators/pbw/model_helpers.rb
+++ lib/generators/pbw/model_helpers.rb
@@ -5,8 +5,6 @@ module Pbw
 		module ModelHelpers
 			include Pbw::Generators::ResourceHelpers
 
-			argument :attributes, :type => :array, :default => [], :banner => "field:type field:type"
-
 			def create_backbone_model
 				template "model.coffee", "#{backbone_path}/models/#{file_name}.js.coffee"
 			end
