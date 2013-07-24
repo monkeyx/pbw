db045becf04decd97e776e1f209465cd123bf426
monkeyx <monkeyx@gmail.com>
2013-07-24 17:07:53 +0100

    Fix model generator

--- lib/generators/pbw/model_generator.rb
+++ lib/generators/pbw/model_generator.rb
@@ -8,7 +8,7 @@ module Pbw
 			argument :attributes, :type => :array, :default => [], :banner => "field:type field:type"
 
 			def create_backbone_model
-				template "model.coffee", "#{backbone_path}/models/#{file_name}.js.coffee"
+				template "model.coffee", "#{backbone_path}/models/#{model_namespace.downcase}/#{file_name}.js.coffee"
 			end
 		end
 	end
