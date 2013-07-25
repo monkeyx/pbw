bea7b580251563941352be50b29885b2d352b4f6
monkeyx <monkeyx@gmail.com>
2013-07-25 17:37:12 +0100

    Fix login

--- app/controllers/pbw/sessions_controller.rb
+++ app/controllers/pbw/sessions_controller.rb
@@ -6,7 +6,7 @@ module Pbw
 			self.resource = warden.authenticate!(auth_options)
 			sign_in(resource_name, resource)
 			::User::Lifecycle.after_login(current_user)
-			respond json: current_user.to_json, status: :ok
+			render json: current_user.to_json, status: :ok
 		end
 
 		def after_sign_out_path_for(resource_name)
