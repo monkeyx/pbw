3c857ea58be686f1a8a003c823685d9ca200681e
monkeyx <monkeyx@gmail.com>
2013-07-25 15:07:18 +0100

    Debugging registrations

--- app/controllers/pbw/registrations_controller.rb
+++ app/controllers/pbw/registrations_controller.rb
@@ -4,17 +4,18 @@ module Pbw
 
 		def create
 			build_resource(params[:user])
-
+			logger.info resource.to_json 
 			if resource.save
 				if resource.active_for_authentication?
-					sign_up(resource_name, resource)
 					Pbw::Engine.user_lifecycle_class.after_signup(current_user)
 					render json: current_user
 				else
+					logger.error "Not active_for_authentication"
 					expire_session_data_after_sign_in!
 					head :no_content
 				end
 			else
+				logger.error "Unable to save resource"
 				clean_up_passwords resource
 				render json: resource.errors.full_messages, status: :unprocessable_entity
 			end
