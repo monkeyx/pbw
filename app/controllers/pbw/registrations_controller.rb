e9ae73325e8d937c878e2a865d2864a019f034d4
monkeyx <monkeyx@gmail.com>
2013-07-25 17:21:21 +0100

    Debug registration

--- app/controllers/pbw/registrations_controller.rb
+++ app/controllers/pbw/registrations_controller.rb
@@ -6,13 +6,8 @@ module Pbw
 			self.resource = User.new(params[:user])
 			logger.error "Password confirmed? #{params[:user][:password] == params[:user][:password_confirmation]}"
 			if resource.save
-				if resource.active_for_authentication?
-					Pbw::Engine.user_lifecycle_class.after_signup(resource)
-					render json: resource
-				else
-					expire_session_data_after_sign_in!
-					head :no_content
-				end
+				Pbw::Engine.user_lifecycle_class.after_signup(resource)
+				render json: resource
 			else
 				render json: resource.errors.full_messages.to_json, status: :unprocessable_entity
 			end
